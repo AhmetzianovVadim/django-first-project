846441fd778e19466c9b918c60cd4b854ed6f073
StepanBezrukov <ahmetzianovvadim@yandex>
2021-03-19 23:42:10 +0300

    Последние правки 1-ой главы

--- bboard/views.py
+++ bboard/views.py
@@ -4,5 +4,5 @@ from .models import Bb
 
 
 def index(request):
-    bbs = Bb.objects.order_by('-published')
-    return render(request, 'bboard/index.html', {'bbs':bbs})
+    bbs = Bb.objects.all()
+    return render(request, 'bboard/index.html', {'bbs': bbs})
